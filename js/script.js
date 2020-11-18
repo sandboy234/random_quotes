6ffb2122b1ee25c54670255a7d22f7220afe6c28
sandboy234 <70071146+sandboy234@users.noreply.github.com>
2020-11-18 14:00:37 -0800

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,52 @@
+/******************************************
+Treehouse FSJS Techdegree:
+project 1 - A Random Quote Generator
+******************************************/
+
+// For assistance:
+  // Check the "Project Resources" section of the project instructions
+  // Reach out in your Slack community - https://treehouse-fsjs-102.slack.com/app_redirect?channel=chit-chat
+
+/***
+ * `quotes` array
+***/
+
+
+
+/***
+ * `getRandomQuote` function
+***/
+
+
+
+/***
+ * `printQuote` function
+***/
+
+
+
+/***
+ * click event listener for the print quote button
+ * DO NOT CHANGE THE CODE BELOW!!
+***/
+var quote = [
+
+  [
+    'quote: the stronger a man is, the more gentle he can afford to be',
+    'author: Elbert hubard'
+  ],
+  [
+    'quote: I dont know which is sadder. a man who thinks he can buy a womans heart, or a woman who lets him',
+    'author: JmStorm'
+  ],
+  [
+    'quote: she loves moonlight and rainstorms and so many other things that have soul',
+    'author: Jmstorm'
+  ],
+  [
+    'quote: I love deadlines. I like the wooshing sound they make as they fly by',
+    'author: Doughlas Adams',
+    'year: April 14, 1982'
+  ]
+]
+console.log(quote)
